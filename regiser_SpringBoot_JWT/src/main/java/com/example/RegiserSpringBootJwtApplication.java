96679bcbfe1c012040029fbcf7d7979a2cbef826
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-06-12 20:58:29 +0530

    Add files via upload

---
+++ regiser_SpringBoot_JWT/src/main/java/com/example/RegiserSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiserSpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiserSpringBootJwtApplication.class, args);
+	}
+
+}
