4a86cc2deac510377895f7baeee88cfaaf17082b
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-06-24 20:37:02 +0530

    Add files via upload

---
+++ regiser_SpringBoot_Batch_JDBC/src/main/java/com/example/RegiserSpringBootBatchJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiserSpringBootBatchJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiserSpringBootBatchJdbcApplication.class, args);
+	}
+
+}
